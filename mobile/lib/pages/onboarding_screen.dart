1065372dba96ab2fd6483d5fc427d28cd84ba32c
anjunthomas <anjunoblethomas@gmail.com>
2025-10-22 15:50:47 -0400

    adding skip and next buttons to  onboarding page and routing from onboarding to register

--- mobile/lib/pages/onboarding_screen.dart
+++ mobile/lib/pages/onboarding_screen.dart
@@ -3,6 +3,7 @@ import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 import '../intro_screens/intro_page_1.dart';
 import '../intro_screens/intro_page_2.dart';
 import '../intro_screens/intro_page_3.dart';
+import 'package:mobile/pages/register_page.dart';
 
 
 class OnBoardingScreen extends StatefulWidget {
@@ -17,6 +18,8 @@ class _OnBoardingScreenState extends State<OnBoardingScreen> {
   // this controller keeps track of what page we're on
   PageController _controller = PageController();
 
+  bool onLastPage = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -24,6 +27,11 @@ class _OnBoardingScreenState extends State<OnBoardingScreen> {
         children: [
           PageView(
             controller: _controller,
+            onPageChanged: (index) {
+              setState(() {
+                onLastPage = (index == 2); // returns true if we're on the last page
+              });
+            },
             children: [
               IntroPage1(),
               IntroPage2(),
@@ -32,7 +40,45 @@ class _OnBoardingScreenState extends State<OnBoardingScreen> {
           ),
           Container(
             alignment: Alignment(0,0.75), // note to self: alignment 0,0 is in the middle
-            child: SmoothPageIndicator(controller: _controller, count: 3)
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                // skip button
+                GestureDetector(
+                  onTap: () {
+                    _controller.jumpToPage(2);
+                  },
+                  child: Text('skip')
+                ),
+                SmoothPageIndicator(controller: _controller, count: 3),
+
+                // next or done if it's the 3rd page
+                onLastPage ?
+                GestureDetector(
+                   onTap: () {
+                    Navigator.push(
+                      context, 
+                      MaterialPageRoute(
+                        builder: (context) {
+                          return RegisterPage();
+                        },
+                      ),
+                    );
+                  },
+                  child: Text('done')
+                ) :
+                
+                GestureDetector(
+                   onTap: () {
+                    _controller.nextPage(
+                      duration: Duration(milliseconds: 500),
+                      curve: Curves.easeIn,
+                    );
+                  },
+                  child: Text('next')
+                )
+              ], 
+            )
             )
         ],
       ),
