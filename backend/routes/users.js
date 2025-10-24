04b5eaef60e456977667588a035469f972526945
AustinVillanueva <avillann00@gmail.com>
2025-10-24 14:54:03 -0400

    feat(Backend): Made minor changes
    features:
    - Updated .gitignore to include package-lock and package
    - Initialized package-lock and package for local testing
    - Moved ModelUser to users/ to keep everything organized

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
-const User = require('../ModelUser');
+const User = require('../models/ModelUser');
 const router = express.Router();
 
 // Generate JWT Token
