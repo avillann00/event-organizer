44b34d7e1739ec4c91a07b3e328071a9c8e7e6d6
Harshika Jindal <mail2harshika10@gmail.com>
2025-10-23 20:26:30 -0400

    [Fix]API: import path for User model in users.js

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
-const User = require('../backend/ModelUser');
+const User = require('../ModelUser');
 const router = express.Router();
 
 // Generate JWT Token
