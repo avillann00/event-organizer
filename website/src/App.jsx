8b450b941af03a887a5cc0d2f2c50b504353c48e
GabrielGimenoAlberti <ga867395@ucf.edu>
2025-10-31 19:01:51 -0400

    downloaded google maps to react

--- website/src/App.jsx
+++ website/src/App.jsx
@@ -1,9 +1,18 @@
-import { Component } from 'react';
+import { useState } from 'react';
+import {
+  APIProvider,
+  Map,
+  AdvancedMarker,
+  Pin,
+  InfoWindow,
+} from '@vis.gl/react-google-maps';
 
-export default class MapComponent extends Component {
-  render() {
-    return(
-      <h1>demo text</h1>
-    )
-  }
+export default function Intro() {
+  const position = { lat: 26.6024, lng: 81.2001 };
+
+  return (
+    <APIProvider apiKey={process.env.GOOGLE_MAPS_API_KEY}>
+      <div>React Google Maps</div>
+    </APIProvider>
+  );
 }
