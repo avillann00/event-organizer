0ed0a80fd450d7a48d11ca78c82d1e2ec36a52c2
GabrielGimenoAlberti <ga867395@ucf.edu>
2025-11-01 00:24:23 -0400

    Implemented google maps w api key

--- website/src/App.jsx
+++ website/src/App.jsx
@@ -7,12 +7,18 @@ import {
   InfoWindow,
 } from '@vis.gl/react-google-maps';
 
-export default function Intro() {
-  const position = { lat: 26.6024, lng: 81.2001 };
-
+export default function App() {
+  console.log('App component is rendering!');
+  console.log('All env vars:', import.meta.env);
+  console.log('API Key:', import.meta.env.VITE_GOOGLE_MAPS_API_KEY);
+  
+  const position = { lat: 28.6024, lng: -81.2001 };
   return (
-    <APIProvider apiKey={process.env.GOOGLE_MAPS_API_KEY}>
-      <div>React Google Maps</div>
+    <APIProvider apiKey={import.meta.env.VITE_GOOGLE_MAPS_API_KEY}>
+      <div style={{height: "100vh"}}>
+        <h1>Testing Map</h1>
+        <Map zoom={15} center={position}></Map>
+      </div>
     </APIProvider>
   );
 }
