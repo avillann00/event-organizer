b18958fde4b537e544b19f0e461191874abc59fa
Dylan-Dansby <djdansby687@gmail.com>
2025-11-03 17:02:36 -0500

    Hi

---
+++ backend/routes/rsvp.js
@@ -0,0 +1,52 @@
+
+const express = require('express');
+const router = express.Router();
+//require jsonwebtoken for token verification
+const jwt = require('jsonwebtoken');
+//So we can query events
+const User = require('../models/ModelEvent');
+//To use values from .env
+require('dotenv').config();
+
+router.put('/', async (req, res) => {
+    try {
+        const { token, eventId, title, description, date } = req.body;
+        //Validate input
+        if (!token || !eventId || !title || !date) {
+            return res.status(400).json({
+                success: false,
+                message: 'Token, event ID, title, description, and date are required to rsvp for events'
+            });
+        }
+        //Verify token
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        //Find event by ID
+        const event = await Event.findById(eventId);
+        if (!event) {
+            return res.status(404).json({
+                success: false,
+                message: 'Event not found'
+            });
+        }
+        //Update event details
+        event.title = title;
+        event.description = description;
+        event.date = date;
+        await event.save();
+        //Send success response
+        res.status(200).json({
+            success: true,
+            message: 'Event rsvp`d successfully',
+            data: {
+                event: event
+            }
+        });
+    } catch (error) {
+        console.error('Error creating rsvp:', error);
+        res.status(500).json({
+            success: false,
+            message: 'Server error while creating rsvp'
+        });
+    }
+});
+module.exports = router;
